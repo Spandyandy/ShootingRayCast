6310f96bed72755058930e0a99332aefd3946c00
Junghoo Kim (Andy) <kimj0@sewanee.edu>
2017-10-17 19:08:38 -0500

    Initial commit

---
+++ Assets/Let's Try Shooter/Scripts/RaycastShoot.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class RaycastShoot : MonoBehaviour
+{
+
+    public int gunDamage = 1;
+
+    public float fireRate = .25f;
+
+    public float weaponRange = 50f;
+
+    public float hitForce = 100f;
+
+    public Transform gunEnd;
+
+
+
+
+
+    private Camera fpsCam;
+
+    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+
+    private AudioSource gunAudio;
+
+    private LineRenderer laserLine;
+
+    private float nextFire;
+
+
+
+    void Start()
+
+    {
+
+
+
+    }
+
+    void Update()
+
+    {
+
+
+
+    }
+
+}
